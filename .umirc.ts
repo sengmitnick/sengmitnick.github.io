c7e99ede16bb16e5faafe30afba6b2e9af343a75
sengmitnick <sengmitnick@163.com>
2021-03-20 21:51:18 +0800

    基于dumi打造的个人博客

---
+++ .umirc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'Seng Mitnick',
+  mode: 'site',
+  copy: ['docs/**/*.jpg'],
+  // more config: https://d.umijs.org/config
+});
